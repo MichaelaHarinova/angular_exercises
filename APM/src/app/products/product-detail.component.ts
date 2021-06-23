8ec4d39dab3cddc4fb9fd7398e9afc8f5937efbb
Micha <michaelaharin@gmail.com>
2021-06-23 13:38:01 +0200

    passed product detail parametrs via route, generated Guardfor routes

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,14 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {IProduct} from "./product";
+import {ProductService} from "./product.service";
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-pageTitle: string = 'Products Detail'
-  constructor() { }
+  pageTitle: string = 'Products Detail'
+  product: IProduct | undefined;
+  errorMessage = '';
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private productService: ProductService) {
   }
 
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    if (id) {
+      this.getProduct(id);
+    }
+  }
+ getProduct(id: number): void{
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => this.errorMessage = err
+    });
+ }
+  onBack(): void {
+    this.router.navigate(['/products']).then(r => console.log());
+  }
 }
